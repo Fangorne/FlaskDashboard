b3429ffff93a28c6627b91e1e34b9596d14f2b98
Marc Ranchin <marcus_the@msn.com>
2025-10-02 18:15:20 +0200

    Update index.sql

--- index.sql
+++ index.sql
@@ -8,3 +8,13 @@ LEFT JOIN sys.dm_db_index_usage_stats s
     AND i.index_id = s.index_id
     AND s.database_id = DB_ID()
 WHERE OBJECTPROPERTY(i.object_id,'IsUserTable') = 1;
+
+
+SELECT 
+    OBJECT_NAME(s.object_id) AS TableName,
+    i.name AS IndexName,
+    STATS_DATE(s.object_id, s.stats_id) AS StatsLastUpdated
+FROM sys.stats s
+JOIN sys.indexes i
+    ON s.object_id = i.object_id
+    AND i.index_id = s.stats_id;
